0f2503160d6cce452b2db48879c5c5a86d3125a5
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-01-10 15:49:33 +0100

    removed useless shit

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,4 @@
 function doStuff(event){
     let i = document.querySelector("i")
     i.classList.toggle("activate")
-    let sidebar = document.querySelector(".sidebar")
-    sidebar.classList.toggle("skit")
 }

08e4079ba20d60c2e27bef01ac1fe20c34e9aaa5
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-01-10 15:44:34 +0100

    gjorde skiten, börja på mobile

---
+++ site/js/main.js
@@ -0,0 +1,6 @@
+function doStuff(event){
+    let i = document.querySelector("i")
+    i.classList.toggle("activate")
+    let sidebar = document.querySelector(".sidebar")
+    sidebar.classList.toggle("skit")
+}
